0819b6db0607d7c9faabcc05701c1e4bc68e740c
ArdianDragusha <ardian.dragusha@myy.haaga-helia.fi>
2025-03-28 17:49:46 +0200

    Lisätty addclothing lomake ja delete painike.
    Lisäsin myös painikkeen clothinglist.html, joka redirectaa addclothing.html lomakkeeseen, sen voi poistaa jos tuntuu ylimääräiseltä

--- sprint1/src/main/java/sprint1/sprint1/web/ClothingItemController.java
+++ sprint1/src/main/java/sprint1/sprint1/web/ClothingItemController.java
@@ -1,19 +1,34 @@
 package sprint1.sprint1.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 
+import sprint1.sprint1.domain.ClothingItem;
 import sprint1.sprint1.domain.ClothingItemRepository;
+import sprint1.sprint1.domain.ManufacturerRepository;
+
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
 
 @Controller
 public class ClothingItemController {
 
-
+   
     private final ClothingItemRepository clothingItemRepository;
 
-    public ClothingItemController(ClothingItemRepository clothingItemRepository) {
+    private final ManufacturerRepository manufacturerRepository;
+
+    public ClothingItemController(ClothingItemRepository clothingItemRepository, 
+            ManufacturerRepository manufacturerRepository) {
         this.clothingItemRepository = clothingItemRepository;
+        this.manufacturerRepository = manufacturerRepository;
     }
     
 
@@ -22,4 +37,26 @@ public class ClothingItemController {
         model.addAttribute("clothings", clothingItemRepository.findAll());
         return "clothinglist";
     }
+
+    @GetMapping("/addclothing")
+    public String addClothing(Model model) {
+        model.addAttribute("clothing", new ClothingItem());
+        model.addAttribute("manufacturers", manufacturerRepository.findAll());
+        return "addclothing";
+    }
+
+    @PostMapping("/addclothing")
+    public String addClothingPost(@ModelAttribute ClothingItem clothingItem) {
+        
+        clothingItemRepository.save(clothingItem);
+        return "redirect:/clothings";
+    }
+   
+    @PostMapping("/clothings/delete/{id}")
+    public String deleteClothing(@PathVariable("id") Long clothingId) {
+        clothingItemRepository.deleteById(clothingId);
+        return "redirect:/clothings";
+    }
+    
+    
 }
