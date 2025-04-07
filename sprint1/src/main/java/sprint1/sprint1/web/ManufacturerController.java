2c24a3e79b035aa7306c9ca7f94d9941528daff6
kkivilahti <bhm585@myy.haaga-helia.fi>
2025-04-07 15:38:17 +0300

    added delete functionality for manufacturers

--- sprint1/src/main/java/sprint1/sprint1/web/ManufacturerController.java
+++ sprint1/src/main/java/sprint1/sprint1/web/ManufacturerController.java
@@ -3,9 +3,12 @@ package sprint1.sprint1.web;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import jakarta.validation.Valid;
 import sprint1.sprint1.domain.Manufacturer;
 import sprint1.sprint1.domain.ManufacturerRepository;
 
@@ -27,9 +30,19 @@ public class ManufacturerController {
         return "addmanufacturer";
     }
 
-    @PostMapping("/manufacturerlist/save")
-    public String saveManufacturer(Manufacturer manufacturer) {
-        mrepository.save(manufacturer);
+    @PostMapping("/savemanufacturer")
+    public String saveManufacturer(@Valid Manufacturer manufacturer, BindingResult result) {
+        if (result.hasErrors()) {
+            return "addmanufacturer";
+        } else {
+            mrepository.save(manufacturer);
+            return "redirect:/manufacturerlist";
+        }
+    }
+
+    @PostMapping("/deletemanufacturer/{id}")
+    public String deleteManufacturer(@PathVariable("id") Long manufacturerId) {
+        mrepository.deleteById(manufacturerId);
         return "redirect:/manufacturerlist";
     }
 }
